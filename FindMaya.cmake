a3b2b319b2c644236d2ec5563453f984d7567223
Daniel Lindsey <dtlindsey@gmail.com>
2015-09-03 00:38:06 -0700

    more fully featured FindMaya.cmake

--- FindMaya.cmake
+++ FindMaya.cmake
@@ -14,6 +14,8 @@ if(NOT DEFINED MAYA_VERSION)
     set(MAYA_VERSION 2015 CACHE STRING "Maya version")
 endif()
 
+# Maya compile defs
+set(MAYA_COMPILE_DEFINITIONS "REQUIRE_IOSTREAM;_BOOL")
 # Maya vars
 set(MAYA_INSTALL_BASE_SUFFIX "")
 set(MAYA_LIB_SUFFIX "lib")
@@ -23,23 +25,32 @@ if(WIN32)
     #Windows
     set(MAYA_INSTALL_BASE "C:/Programs Files/Autodesk")
     set(OPENMAYA OpenMaya.lib)
+    set(MAYA_COMPILE_DEFINITIONS "${MAYA_COMPILE_DEFINITIONS};NT_PLUGIN")
+    set(MAYA_PLUGIN_EXTENSION ".mll")
 elseif(APPLE)
     #Mac
     set(MAYA_INSTALL_BASE "/Applications/Autodesk")
     set(MAYA_LIB_SUFFIX "Maya.app/Contents/MacOS")
     set(MAYA_INC_SUFFIX "devkit/include")
     set(OPENMAYA libOpenMaya.dylib)
+    set(MAYA_COMPILE_DEFINITIONS "${MAYA_COMPILE_DEFINITIONS};OSMac_")
+    set(MAYA_PLUGIN_EXTENSION ".bundle")
 else()
     #Linux
     set(MAYA_INSTALL_BASE "/usr/autodesk")
     set(MAYA_INSTALL_BASE_SUFFIX "-x64")
     set(OPENMAYA libOpenMaya.so)
+    set(MAYA_COMPILE_DEFINITIONS "${MAYA_COMPILE_DEFINITIONS};LINUX")
+    set(MAYA_PLUGIN_EXTENSION ".so")
+    # only needed for linux
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fPIC")
 endif()
 
 set(MAYA_INSTALL_BASE_PATH ${MAYA_INSTALL_BASE} CACHE STRING 
     "Root Maya install path, e.g. /usr/autodesk")
 
-set(MAYA_LOCATION ${MAYA_INSTALL_BASE_PATH}/maya/${MAYA_VERSION}${MAYA_INSTALL_BASE_SUFFIX})
+set(MAYA_LOCATION 
+    ${MAYA_INSTALL_BASE_PATH}/maya/${MAYA_VERSION}${MAYA_INSTALL_BASE_SUFFIX})
 
 #find_path is a cmake built-in
 find_path(MAYA_LIBRARY_DIR ${OPENMAYA}
@@ -59,13 +70,30 @@ find_path(MAYA_INCLUDE_DIR maya/MFn.h
     DOC "Maya include path")
 
 # Maya libs
-set(_MAYA_LIBRARIES OpenMaya OpenMayaAnim OpenMayaFX OpenMayaRender OpenMayaUI foundation)
+set(_MAYA_LIBRARIES OpenMaya OpenMayaAnim OpenMayaFX OpenMayaRender OpenMayaUI 
+    foundation)
 foreach(MAYA_LIB ${_MAYA_LIBRARIES}
-        find_library(MAYA_${MAYA_LIB}_LIBRARY NAMES ${MAYA_LIB} PATHS ${MAYA_LIBRARY_DIR} 
-	    NO_DEFAULT_PATH)
+        find_library(MAYA_${MAYA_LIB}_LIBRARY NAMES ${MAYA_LIB} 
+	    PATHS ${MAYA_LIBRARY_DIR} NO_DEFAULT_PATH)
         set(MAYA_LIBRARIES ${MAYA_LIBRARIES} ${MAYA_${MAYA_LIB}_LIBRARY})
 endforeach()
 
 # Maya includs
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Maya DEFAULT_MSG MAYA_INCLUDE MAYA_LIBRARIES)
+
+# target function
+function(MAYA_PLUGIN _target)
+  # windows needs extra linking flags
+  if (WIN32)
+    set_target_properties(${_target} PROPERTIES
+      LINK_FLAGS "/export:intializePlugin /export:uninitializePlugin")
+  endif()
+  #
+  set_target_properties(${_target} PROPERTIES
+    COMPILE_DEFINITIONS "${MAYA_COMPILE_DEFINITIONS}"
+    # this is so mac and linux don't prepend lib to the head
+    PREFIX ""
+    SUFFIX ${MAYA_PLUGIN_EXTENSION}
+    )
+endfunction()
